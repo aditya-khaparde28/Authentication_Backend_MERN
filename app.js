e703cd600cc97bad133513afdcdbfc23ba63a996
Aditya Khaparde <adityakhaparde@Adityas-Laptop.local>
2024-07-04 00:58:46 +0530

    added middlewere

--- app.js
+++ app.js
@@ -25,8 +25,24 @@ app.use(
             }
         },
         credentials: true, // Allow credentials
+        optionsSuccessStatus: 200 
     })
 );
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    }
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    next();
+});
 app.use(cookieParser());
 
 app.use('/auth',authRoutes);
